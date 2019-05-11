6f09ca5b8ebdb15d47e0c5741caead2b34d5c1cf
ZuiQuanZ <123casper@mail.ru>
2019-05-11 03:02:31 +0300

    WTF?!

--- src/main/java/jm/student/secutiry/config/SecurityConfig.java
+++ src/main/java/jm/student/secutiry/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import jm.student.secutiry.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,51 +15,70 @@ import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
 @Configuration
-@ComponentScan("jm.student")
-@EnableWebSecurity
+//@ComponentScan("jm.student")
+//@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final UserDetailsServiceImpl authService;
-    private final AuthSuccessHandler authSuccessHandler;
-    private final AuthFailHandler authFailHandler;
-    private final LogoutSuccessHandler logoutSuccessHandler;
+//    private final UserDetailsServiceImpl authService;
+//    private final AuthSuccessHandler authSuccessHandler;
+//    private final AuthFailHandler authFailHandler;
+//    private final LogoutSuccessHandler logoutSuccessHandler;
+//
+//    @Autowired
+//    public SecurityConfig(UserDetailsServiceImpl authService,
+//                          AuthSuccessHandler authSuccessHandler,
+//                          AuthFailHandler authFailHandler,
+//                          LogoutSuccessHandler logoutSuccessHandler) {
+//        this.authService = authService;
+//        this.authSuccessHandler = authSuccessHandler;
+//        this.authFailHandler = authFailHandler;
+//        this.logoutSuccessHandler = logoutSuccessHandler;
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+//        filter.setEncoding("UTF-8");
+//        filter.setForceEncoding(true);
+//        http.csrf().disable().addFilterBefore(filter, CsrfFilter.class);
+//        http.authorizeRequests()
+//                .antMatchers("/user/**").hasAnyAuthority("USER")
+//                .antMatchers("/admin/**").hasAnyAuthority("ADMIN")
+//                .and()
+//                .formLogin()
+//                .loginPage("/login")
+//                .loginProcessingUrl("/processing-url")
+//                .successHandler(authSuccessHandler)
+//                .failureHandler(authFailHandler)
+//                .usernameParameter("login")
+//                .passwordParameter("password")
+//                .and()
+//                .logout()
+//                .logoutUrl("/logout")
+//                .logoutSuccessUrl("/login?logout")
+//                .invalidateHttpSession(true)
+//                .logoutSuccessHandler(logoutSuccessHandler)
+//                .permitAll()
+//                .and().exceptionHandling().accessDeniedPage("/error");
+//    }
+@Override
+protected void configure(HttpSecurity http) throws Exception
+{
+    http
+            .csrf().disable()
+            .authorizeRequests().anyRequest().authenticated()
+            .and()
+            .httpBasic();
+}
 
     @Autowired
-    public SecurityConfig(UserDetailsServiceImpl authService,
-                          AuthSuccessHandler authSuccessHandler,
-                          AuthFailHandler authFailHandler,
-                          LogoutSuccessHandler logoutSuccessHandler) {
-        this.authService = authService;
-        this.authSuccessHandler = authSuccessHandler;
-        this.authFailHandler = authFailHandler;
-        this.logoutSuccessHandler = logoutSuccessHandler;
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception
+    {
+        auth
+                .inMemoryAuthentication()
+                .withUser("admin")
+                .password("admin")
+                .roles("ADMIN");
     }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        CharacterEncodingFilter filter = new CharacterEncodingFilter();
-        filter.setEncoding("UTF-8");
-        filter.setForceEncoding(true);
-        http.csrf().disable().addFilterBefore(filter, CsrfFilter.class);
-        http.authorizeRequests()
-                .antMatchers("/user/**").hasAnyAuthority("USER")
-                .antMatchers("/admin/**").hasAnyAuthority("ADMIN")
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .loginProcessingUrl("/processing-url")
-                .successHandler(authSuccessHandler)
-                .failureHandler(authFailHandler)
-                .usernameParameter("login")
-                .passwordParameter("password")
-                .and()
-                .logout()
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/login?logout")
-                .invalidateHttpSession(true)
-                .logoutSuccessHandler(logoutSuccessHandler)
-                .permitAll()
-                .and().exceptionHandling().accessDeniedPage("/error");
-    }
-
 }
